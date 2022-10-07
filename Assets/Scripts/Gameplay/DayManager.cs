96915761eee22434b888431c3ebff647b1c94821
diotsani <dio.tsani123@gmail.com>
2022-10-07 16:45:27 +0700

    update day manager

---
+++ Assets/Scripts/Gameplay/DayManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+namespace Team8.Unemployment.Gameplay
+{
+    public class DayManager : MonoBehaviour
+    {
+        public delegate void EventName();
+        public static event EventName OnMaxDay;
+        
+        public delegate void EventParameter(int value);
+        public static event EventParameter OnShowDay;
+        
+        [SerializeField] private int _amountDay;
+        private int _maxDay = 7;
+        private bool isMaxDay = false;
+
+        private void Start()
+        {
+            ChangeDay();
+        }
+        private void ChangeDay()
+        {
+            CheckDay();
+            if (!isMaxDay)
+            {
+                _amountDay++;
+                OnShowDay?.Invoke(_amountDay);
+            }
+        }
+        private void CheckDay()
+        {
+            if (_amountDay == _maxDay)
+            {
+                isMaxDay = true;
+                OnMaxDay?.Invoke();
+            }
+        }
+    }
+}
--- Assets/Scripts/Gameplay/GameplayView.cs
+++ Assets/Scripts/Gameplay/GameplayView.cs
@@ -1,9 +1,36 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
 
 namespace Team8.Unemployment.Gameplay
 {
     public class GameplayView : MonoBehaviour
     {
-        
+        [Header("Day Display")]
+        [SerializeField] private TMP_Text _dayText;
+        [SerializeField] private GameObject _dayPanel;
+
+        private void OnEnable()
+        {
+            DayManager.OnShowDay += ShowDay;
+        }
+
+        private void OnDisable()
+        {
+            DayManager.OnShowDay -= ShowDay;
+        }
+
+        private void ShowDay(int value)
+        {
+            _dayText.text = Constants.Status.Day + value.ToString();
+            _dayPanel.SetActive(true);
+            StartCoroutine(AfterActive(_dayPanel,2f));
+        }
+        private IEnumerator AfterActive(GameObject obj, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            obj.SetActive(false);
+        }
     }
 }
